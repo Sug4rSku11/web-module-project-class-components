dd6d3aabe15e87261fed1c05534d03b034c7ba49
Desiree Garcia <desireegarcia@Macintosh.hsd1.nm.comcast.net>
2021-10-25 18:02:51 -0600

    added code to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import '../src/components/Todo.css';
+
+
+const todo = [
+  {
+  name: 'Buy Groceries',
+  id: 1,
+  completed: false
+},
+{
+  name: 'Buy Dog Food',
+  id: 2,
+  completed: false
+}
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todo: todo
+    }
+  }
+  //Clear done
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todo: this.state.todo.filter(item => !item.todo)
+    });
+  }
+  //Add Item
+  handleAddItem = (item) => {
+  const newItem = {
+    name: item,
+    id: Date.now(),
+    completed: false
+  };
+    this.setState({
+      ...this.state,
+      todo: [...this.state.todo, newItem]
+  });
+}
+handleToggleItem = (item) => {
+  this.setState({
+  ...this.state,
+  todo: this.state.todo.map(todo => {
+    if (todo.id === item.id) {
+      return {
+        ...todo,
+        completed: !todo.completed
+      }
+    }
+    return todo;
+  })
+});
+}
+
   render() {
     return (
       <div>
@@ -12,6 +67,7 @@ class App extends React.Component {
       </div>
     );
   }
-}
+  }
+
 
 export default App;
